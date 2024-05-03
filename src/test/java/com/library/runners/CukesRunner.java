60a95f025f3eb8c82e6335581f3f02d8af091612
Lucy Subota <lucysubota@gmail.com>
2024-05-03 15:57:43 -0500

    changes

---
+++ src/test/java/com/library/runners/CukesRunner.java
@@ -0,0 +1,24 @@
+package com.library.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        plugin = {
+                "html:target/cucumber-reports.html",
+                "rerun:target/rerun.txt",
+                "json:target/cucumber.json",
+                "pretty"
+        },
+        features = "src/test/resources/features",
+        glue = "com/library/step_definitions",
+        dryRun = false,
+        tags = "@api_sprint"
+        // publish = true, // Enable publishing of test results
+        // publishUrl = "https://reporting-server.example.com" // Specify the URL of the reporting server
+
+)
+public class CukesRunner {}
